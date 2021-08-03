68dcbebd9e1badfbf2bd77499ab5d990cba52b34
Kaustubh Manglurkar <kaustubh1517@gmail.com>
2021-08-03 12:42:41 +0530

    app done

---
+++ app.js
@@ -0,0 +1,56 @@
+const birthDate = document.querySelector("#birth-date");
+
+const luckyNumber = document.querySelector("#lucky-number");
+
+const checkButton = document.querySelector("#check-button");
+
+const outputBox = document.querySelector("#output");
+
+
+
+function checkHandler() {
+
+   let dob = birthDate.value;
+
+  var sum = calculateSum(dob);
+
+  compareValues(sum, luckyNumber.value);
+
+}
+
+function calculateSum(dob){
+
+    dob = dob.replaceAll("-","");
+
+    let sum = 0;
+
+    for(var i = 0; i < dob.length; i++){
+
+        sum = sum + Number(dob.charAt(i));
+
+    }
+
+    return sum;
+
+}
+
+function compareValues(sum, luckyNumber) {
+
+    if(sum % luckyNumber === 0){
+
+    outputBox.innerText = "Yay, your birthday is lucky 🚀!"
+
+        
+    }
+
+    else {
+
+        outputBox.innerText = "Sorry, your birthday is not lucky 😢"
+
+    }
+
+
+}
+
+
+checkButton.addEventListener("click", checkHandler)
